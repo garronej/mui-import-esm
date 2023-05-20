616deb5606035485634d9c059c7a42061c338cdb
garronej <joseph.garrone@data.gouv.fr>
2023-05-20 03:08:55 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # mui-import-esm
-Demo repository showing that emotion can't be imported as an ESM module.
+
+```bash
+yarn
+node index.mjs
+```
---
+++ index.mjs
@@ -0,0 +1,5 @@
+import { useTheme } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+
+console.log(useTheme);
+console.log(Button); // This is an object with a default property instead of a function
